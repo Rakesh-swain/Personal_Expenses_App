bd30b320fab917917d5d8fc41f0f721d947ca6a0
Rakesh005 <srakeshswain005@gmail.com>
2022-05-05 00:45:00 +0530

    create bars for chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -18,7 +18,10 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
   }
 
@@ -28,8 +31,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children: [],
-      ),
+          children: groupedTransactionValues.map((data) {
+        return Text('${data['day']}: ${data['amount']}');
+      }).toList()),
     );
   }
 }
