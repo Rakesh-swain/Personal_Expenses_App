1d2b409a4eca732a2877951da936c26041a392e0
Rakesh005 <srakeshswain005@gmail.com>
2022-05-05 21:28:31 +0530

    improve the overall chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,10 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+        Container(
+            height: 20,
+            child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}'))),
         const SizedBox(
           height: 4,
         ),
