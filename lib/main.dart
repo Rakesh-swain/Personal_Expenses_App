7b27d4b9e042d8705e72f9a017d1a0374e364b35
Rakesh005 <srakeshswain005@gmail.com>
2022-05-21 13:15:58 +0530

    check the device platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './widgets/chart.dart';
@@ -141,7 +143,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text('Show Chart'),
-                    Switch(
+                    Switch.adaptive(
+                        activeColor: Theme.of(context).accentColor,
                         value: _showChart,
                         onChanged: (val) {
                           setState(() {
@@ -173,10 +176,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ]),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+            ),
     );
   }
 }
